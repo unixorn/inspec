4984328eb74df2676b12ec715f0841724daea5f7
Dominik Richter <dominik.richter@gmail.com>
2016-09-14 02:08:26 +0200

    add tutorial

---
+++ tutorial/app/app.component.ts
@@ -0,0 +1,247 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { XtermTerminalComponent } from './xterm-terminal/xterm-terminal.component';
+declare var require: any;
+var shellwords = require("shellwords");
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  providers: [ HTTP_PROVIDERS ],
+  directives: [ XtermTerminalComponent ]
+})
+
+export class AppComponent implements OnInit {
+  // template values
+  instructions: any;
+  title: string;
+
+  // taken as input by xterm terminal componen
+  response: string;
+  shell: string;
+
+  // colors for responses
+  red: string = "[31m";
+  white: string = "[37m";
+  black: string = "[30m";
+
+  matchFound: boolean; // helps to handle no match found response
+  counter: number = 0; // keeps track of step number count
+  userCommand: string; // used to display better error msg when no match is found
+  extraCmds: string = ''; // used to display extra commands available (from commands.yml)
+
+  // arrays of data parsed from json files
+  commandsArray: any = [];
+  instructionsArray: any = [];
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    // load json files
+    this.getInstructions();
+    this.getResponses();
+    this.getExtraCmds();
+  }
+
+  ngAfterViewChecked() {
+    window.scrollTo( 0, document.body.scrollHeight );
+  }
+
+  // called when command entered is 'next' or 'prev'
+  // modifies value of counter and calls displayInstructions
+  updateInstructions(step) {
+    let totalSteps = this.instructionsArray.length - 1;
+    let msg = Math.random();
+    if (step === 'next') {
+      if (this.counter < totalSteps) {
+        this.counter += 1;
+      }
+      this.response = this.black + 'next' + msg;
+    } else if (step === 'prev') {
+      if (this.counter > 0) {
+        this.counter -= 1;
+      }
+      this.response = this.black + 'prev' + msg;
+    }
+    this.displayInstructions();
+  }
+
+  // display instructions based on value of counter and
+  // format text to remove triple backticks. if the user has reached
+  // then end of the demo, display a message containing extra commands that have been
+  // enabled in the demo
+  displayInstructions() {
+    if (this.counter === this.instructionsArray.length - 1) {
+      this.title = "the end; that's all folks!";
+      this.instructions = "here are some other commands you can try out: \r\n\r\n" + this.extraCmds;
+    } else {
+      if (this.instructionsArray[this.counter][1]) {
+        this.title = this.instructionsArray[this.counter][0];
+        this.instructions = this.instructionsArray[this.counter][1];
+      } else {
+        this.instructions = 'Sorry, something seems to have gone wrong. Please try refreshing your browser.';
+      }
+    }
+  }
+
+  formatInstructions() {
+    return this.instructions || '';
+  }
+
+  // called when a new value is emitted for command
+  // checks for a match, calls parseInspecShell if shell is inspec-shell
+  // and calls checkCommand if none of the first commands match
+  evalCommand(command) {
+    this.userCommand = command;
+    if (command.match(/^next\s*/)) {
+      this.updateInstructions('next');
+    }
+    else if (command.match(/^prev\s*/)) {
+      this.updateInstructions('prev');
+    }
+    else if (this.shell === 'inspec-shell') {
+      this.parseInspecShell(command);
+    }
+    else if (command.match(/^ls\s*/)) {
+      this.response = this.white + "README.md";
+    }
+    else if (command.match(/^pwd\s*/)) {
+      this.response = this.white + "anonymous-web-user/inspec";
+    }
+    else if (command.match(/^cat\s*README.md\s*/i)) {
+      this.response = this.white + "Only a few commands are implemented in this terminal.  Please follow the demo";
+    }
+    else if (command.match(/^less\s*README.md\s*/i)) {
+      this.response = this.white + "Only a few commands are implemented in this terminal.  Please follow the demo.";
+    }
+    else {
+      this.checkCommand(command);
+    }
+  }
+
+  // if the shell is inspec-shell, we want to exit the shell env on 'exit'
+  // format the command for checkCommand by using shellwords.escape and
+  // adding 'echo' and 'inspec shell' to match command from commands.json
+  parseInspecShell(command) {
+    if (command.match(/^exit\s*/))  {
+      this.shell = '';
+      this.response = '';
+    }
+    else if (command.match(/^pwd\s*/)) {
+      this.response = this.white + "anonymous-web-user/inspec-shell";
+    }
+    else {
+      let escaped_cmd;
+      let formatted_cmd;
+      // TODO: make this better
+      // I don't really like what we're doing here when we have a
+      // describe and control block, but I had a lot of trouble getting this
+      // to work because of the new lines carriage returns in the command from commands.json
+      // so at the moment we're splitting the command on "do" and assuming a match if that first
+      // result group matches (everything before do) :/
+      if (command.match(/^describe.*|^control/)) {
+        let split_cmd = command.split('do');
+        escaped_cmd = shellwords.escape(split_cmd[0]);
+        formatted_cmd = 'echo.*' + escaped_cmd ;
+      } else {
+        escaped_cmd = shellwords.escape(command)
+        formatted_cmd = 'echo.*' + escaped_cmd + '.*inspec.*shell';
+      }
+      let regex_compatible = formatted_cmd.replace(/\W+/g, '.*');
+      this.checkCommand(regex_compatible);
+    }
+  }
+
+  // takes the command as input, replaces all white space with regex whitespace matcher
+  // and creates a new regex. check if the regex matches any of the keys in the commandsArray
+  // if it matches, we set matchFound to true and call displayResult. if it doesn't match,
+  // we display a default error message
+  checkCommand(command) {
+    let dir = 'app/responses/';
+    let cmd = command.replace(/ /g,"\\s*")
+    let regexcmd = new RegExp(('^'+cmd+'$'), 'm')
+    this.matchFound = false;
+    for (var i = 0; i < this.commandsArray.length; i++) {
+      let object = this.commandsArray[i]
+      for (let key in object) {
+        if (key.match(regexcmd)) {
+          this.matchFound = true;
+          let value = object[key];
+          this.http.get(dir + value).subscribe(data => {
+            this.displayResult(command, data);
+          },
+          err => console.error(err));
+        }
+      }
+    }
+    // if no match is found, we check if the command entered was inspec exec something
+    // and if it is respond appropriately ('could not fetch inspec profile in ''), otherwise
+    // respond with 'invalid command' and the command entered
+    if (this.matchFound === false) {
+      let msg = Math.random();
+      if (command.match(/^inspec exec\s*.*/)) {
+        let target = command.match(/^inspec exec\s*(.*)/)
+        if (target[1].match(/.*-t.*|.*-b.*/)) {
+          this.response = this.red + "Sorry, we haven't figured out how to handle the transport options in this demo just yet." + this.black + msg;
+        } else {
+          this.response = this.red + "Could not fetch inspec profile in '" + target[1] + "' " + this.black + msg;
+        }
+      } else {
+        this.response = this.red + 'invalid command: ' + this.userCommand + this.black + msg;
+      }
+    }
+  }
+
+  // if the command if inspec shell, we also need to set the shell variable to
+  // inspec shell. print response value and random msg (to ensure recognition of value change by xterm
+  // terminal component)
+  displayResult(command, data) {
+    if (command.match(/^inspec\s*shell\s*$/)) {
+      this.shell = 'inspec-shell';
+    }
+    let msg = Math.random();
+    this.response = this.white + data['_body'] + this.black + msg;
+  }
+
+  // load json file for instructions and save to instructionsArray
+  // call displayInstructions to load first set of instructions
+  getInstructions() {
+    this.http.get('tutorial_files/instructions.json')
+      .subscribe(data => {
+        this.instructionsArray = JSON.parse(data['_body']);
+        this.displayInstructions();
+      },
+      err => console.error(err)
+    );
+  }
+
+  // load json file for commands and push each object to commandsArray
+  getResponses() {
+    this.http.get('tutorial_files/commands.json')
+      .subscribe(data => {
+        let result = JSON.parse(data['_body']);
+        for (var i = 0; i < result.length; i++) {
+          let object = result[i][0];
+          this.commandsArray.push(object);
+        }
+      },
+      err => console.error(err)
+    );
+  }
+
+  // load json file for extra commands to display extra commands available to user
+  getExtraCmds() {
+    this.http.get('tutorial_files/extra_commands.json')
+      .subscribe(data => {
+        let result = JSON.parse(data['_body']);
+        for (var i = 0; i < result.length; i++) {
+          this.extraCmds += result[i] + "\r\n";
+        }
+      },
+      err => console.error(err)
+    );
+  }
+}
---
+++ tutorial/app/xterm-terminal/xterm-terminal.component.ts
@@ -0,0 +1,226 @@
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChange } from '@angular/core';
+declare var Terminal: any;
+
+@Component({
+  selector: 'xterm-terminal',
+  templateUrl: 'app/xterm-terminal/xterm-terminal.component.html',
+  styleUrls: ['app/xterm-terminal/xterm-terminal.component.css']
+})
+export class XtermTerminalComponent implements OnInit {
+  @Input() response: string;
+  @Input() shell: string;
+  @Output() command: EventEmitter<string> = new EventEmitter<string>();
+
+  // handle up/down arrow functionality
+  previousCommands: any = [];
+  poppedCommands: any = [];
+  last: number;
+
+  // handle user input
+  buffer: string = ''; // the buffer is our string of user input
+  blockCmd: string = ''; // handle describe and control blocks
+
+  // xterm variables
+  terminalContainer: any;
+  term: any;
+  optionElements: any;
+  cols: string;
+  rows: string;
+
+  ngOnInit() {
+    // set up Xterm terminal (https://github.com/sourcelair/xterm.js)
+    this.terminalContainer = document.getElementById('terminal-container');
+    this.cols = '70';
+    this.rows = '70';
+    this.createTerminal();
+   }
+
+  // watch for changes on the value of response and call printResponse
+  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+    if (changes['response'] && this.term) {
+      let currentResponse = changes['response'].currentValue;
+      this.printResponse(currentResponse);
+    }
+  }
+
+  // create Xterm terminal (https://github.com/sourcelair/xterm.js)
+  createTerminal() {
+    while (this.terminalContainer.children.length) {
+      this.terminalContainer.removeChild(this.terminalContainer.children[0]);
+    }
+    this.term = new Terminal({
+      cursorBlink: true
+    });
+
+    this.term.open(this.terminalContainer);
+    this.term.fit();
+
+    var initialGeometry = this.term.proposeGeometry(),
+      cols = initialGeometry.cols,
+      rows = initialGeometry.rows;
+
+    this.cols = cols;
+    this.rows = rows;
+    this.runFakeTerminal()
+  }
+
+  // more Xterm terminal stuff. we're faking it. faking it can be a good thing ;)
+  runFakeTerminal() {
+    if (this.term._initialized) {
+      return;
+    }
+    this.term._initialized = true;
+    this.setPrompt();
+  }
+
+  // if the response matches the special message we send when the user
+  // has entered 'next' or 'prev' (to navigate through the demo)
+  // then no need to print the response, just set the prompt. otherwise,
+  // print response and set prompt
+  printResponse(response) {
+    if (response.match(/.30mnext|.30mprev/)) {
+      // call createTerminal to clear terminal screen on next and prev
+      this.createTerminal();
+    } else {
+      this.term.writeln(response);
+      this.setPrompt();
+    }
+  }
+
+  // the value of shell is taken as an input. the default
+  // shellprompt is displayed unless shell is set to 'inspec-shell'
+  setPrompt() {
+    this.buffer = '';
+    if (this.shell === 'inspec-shell') {
+      if (this.blockCmd != '') {
+        this.term.write(' [32minspec>*'); // green inspec shell prompt with * to denote multi-line command
+      } else {
+        this.term.write(' [32minspec> '); // green inspec shell prompt
+      }
+    } else {
+      this.term.write('[36m$ '); // blue regular shell prompt
+    }
+  }
+
+  // delete everything on the line
+  deleteCharacters() {
+    // don't delete the prompt
+    let letters = this.term.x - 2;
+    for (var i = 0; i < letters; i++) {
+      this.term.write('\b \b');
+    }
+  }
+
+  // keydown calls handleDelete to check if the user is holding down the backpace key
+  // TODO: make this work! the backspace event isn't coming in for some reason :(
+  handleDelete(ev) {
+    if (ev.keyCode == 8) {
+      this.deleteCharacters();
+    }
+  }
+
+  // handle describe and control blocks. if the command entered matches the
+  // syntax we would generally expect in a control/describe block, then we
+  // wait to have the whole string command (as marked by 'end' for a simple describe
+  // block and end end for a control block) and emit that
+  handleBlockCommand(buffer) {
+    this.blockCmd += buffer + '';
+    if (this.blockCmd.match(/^control/)) {
+      if (this.blockCmd.match(/end.*end.*/)) {
+        this.command.emit(this.blockCmd);
+        this.blockCmd = '';
+      } else {
+        this.setPrompt();
+      }
+    } else {
+      if (buffer.match(/^end$/)) {
+        this.command.emit(this.blockCmd);
+        this.blockCmd = '';
+      } else {
+        this.setPrompt();
+      }
+    }
+  }
+
+  // onKey will check if the character is printable (on keyup) and add it to the buffer. if the enter key is hit
+  // the value of the buffer is emitted as 'command'.  onKey also supports proper backspace handling and
+  // the ability to up-arrow through previous commands/down arrow back through them.
+  onKey(ev) {
+    var shell = null
+    // determine if the character is a printable one
+    var printable = ['Alt', 'Control', 'Meta', 'Shift', 'CapsLock', 'Tab', 'Escape', 'ArrowLeft', 'ArrowRight'].indexOf(ev.key) == -1
+
+    // on enter, save command to array and send current value of buffer
+    // to parent component (app)
+    if (ev.keyCode == 13) {
+      if ((this.buffer === 'clear') || (this.buffer === 'cls')) {
+        this.createTerminal();
+      } else {
+        this.previousCommands.push(this.buffer);
+        this.term.write('\r\n');
+        if (this.shell === 'inspec-shell') {
+          // if the command entered matches any of the typical describe and
+          // control blocks, then we call handleBlockCommand to not emit the
+          // value until we have the whole multi-line command
+          if (this.buffer.match(/^describe.*|^it.*|^end$|^impact.*|^title.*|^control.*/)) {
+            this.handleBlockCommand(this.buffer);
+          } else {this.command.emit(this.buffer);}
+        } else {
+          this.command.emit(this.buffer);
+        }
+      }
+    }
+    // on backspace, pop characters from buffer
+    else if (ev.keyCode == 8) {
+      // if inspec shell is being used, this needs to be set to 9 to account for the extra letters
+      if (this.shell === 'inspec-shell') {
+        if (this.term.x > 8) {
+          this.buffer = this.buffer.substr(0, this.buffer.length-1);
+          this.term.write('\b \b');
+        }
+      } else {
+        // setting the value here to 3 ensures that we don't delete the promp '$' or the space after it
+        if (this.term.x > 2) {
+          this.buffer = this.buffer.substr(0, this.buffer.length-1);
+          this.term.write('\b \b');
+        }
+      }
+    }
+    // on up arrow, delete anything on line, print previous command
+    // and push last to poppedCommands
+    else if (ev.keyCode === 38) {
+      let last;
+      this.buffer = '';
+      if (this.previousCommands.length > 0) {
+        last = this.previousCommands.pop();
+        this.poppedCommands.push(last);
+      } else {
+        last = '';
+      }
+      this.deleteCharacters();
+      this.buffer = last;
+      this.term.write(last);
+    }
+    // on down arrow, delete anything on line, print last item from poppedCommands
+    // and push previous to previousCommands
+    else if (ev.keyCode === 40) {
+      let previous;
+      this.buffer = '';
+      if (this.poppedCommands.length > 0) {
+        previous = this.poppedCommands.pop();
+        this.previousCommands.push(previous);
+      } else {
+        previous = '';
+      }
+      this.deleteCharacters();
+      this.buffer = previous;
+      this.term.write(previous);
+    }
+    // if the character is printable and none of the event key codes from above were matched
+    // then we write the character on the prompt line and add the character to the buffer
+    else if (printable) {
+      this.term.write(ev.key);
+      this.buffer += ev.key;
+    }
+  }
+}
